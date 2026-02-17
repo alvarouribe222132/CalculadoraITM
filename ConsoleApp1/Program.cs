54c1f392b5b32d00f94c405d5cf8ac797e06c056
Alvaro Uribe <206965282+alvarouribe222132@users.noreply.github.com>
2026-02-17 14:22:23 -0500

    Importar proyecto local al repositorio CalculadoraITM

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace Calculadora
+{
+	internal class program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+	
+}
