5b7b5256a586619524015fe616accdeff182ddc0
Alvaro Uribe <206965282+alvarouribe222132@users.noreply.github.com>
2026-02-17 20:00:06 -0500

    se corrige errores lógicos, simbología, se acomoda correctamente el break en la condicional y se corrige identacion y palabras en formato camelCase

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Reflection;
 
 namespace Calculadora
 {
@@ -23,28 +24,47 @@ namespace Calculadora
 			//verificacion si el numero ingresado es valido
 			double.TryParse(Console.ReadLine(), out num2);
 
-			//Pregunta si 
-			//Console.Write("Desea ingresar mas Numeros ?");
-			//condicional en caso que el usuario desee ingresar mas numeros 
-
-			//2 MODULO
+			// 2 MODULO
 			//Seleccionar que  tipo de operacion desea realizar 
-			Console.WriteLine("\nSeleccione la Operación (+, -, *, /):");
-			string operacion = Console.ReadLine();
-			double Resultado = 0;
+			const int MaxIntentos = 5;
+			string operacion = null;
 
+			for (int intento = 1; intento <= MaxIntentos; intento++)
+			{
+				Console.WriteLine("\nSeleccione la Operación (+, -, *, /):");
+				string entrada = Console.ReadLine()?.Trim();
+
+				if (entrada == "+" || entrada == "-" || entrada == "*" || entrada == "/")
+				{
+					operacion = entrada;
+					break;
+				}
+
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine($"Opcion No Permitida use solo los operadores + - * /  \n Intento {intento} de {MaxIntentos} \n Try Again");
+				Console.ResetColor();
+			}
+
+			if (operacion == null)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Se Agotaron los Permisos.  El Programa se Cerrará. ");
+				Console.ResetColor();
+				return;
+			}
+			double resultado = 0;
 
 			//3 MODULO
 			switch (operacion)
 			{
 				case "+":
-					Resultado = num1 + num2;
+					resultado = num1 + num2;
 					break;
 				case "-":
-					Resultado = num1 - num2;
+					resultado = num1 - num2;
 					break;
 				case "*":
-					Resultado = num1 * num2;
+					resultado = num1 * num2;
 					break;
 				case "/":
 					//verificacion dvision por cero
@@ -57,7 +77,7 @@ namespace Calculadora
 
 					}
 					else
-						Resultado = num1 / num2;
+						resultado = num1 / num2;
 					break;
 
 				default:
@@ -67,17 +87,36 @@ namespace Calculadora
 					return;
 
 			}
+
+
+			//Pregunta si 
+			//Console.Write("Desea ingresar mas Numeros ?");
+			//condicional en caso que el usuario desee ingresar mas numeros 
+
+
+
 			//4 MODULO
 			//Salida de Resultados 
 			Console.ForegroundColor = ConsoleColor.Green;
-			Console.WriteLine($"\nRESULTADO: {num1} {operacion} {num2} = {Resultado}");
+			Console.WriteLine($"\nRESULTADO: {num1} {operacion} {num2} = {resultado}");
 
 			//resetear para dejar consola limpia
 			Console.ResetColor();
 
 			//Esperar hasta que el usuario cierre la ventana 
-			Console.WriteLine("\nDesea Realizar otra operacion? ");
-			Console.WriteLine("\nPresione S para continuar o Presione C para cancelar y salir");
+			Console.WriteLine("\n ¿Desea Realizar otra operación? ");
+			Console.WriteLine("\nPresione S = continuar | C = cancelar y salir");
+			string continuar = Console.ReadLine()?.ToUpperInvariant();
+
+			if (continuar == "S")
+			{
+				Console.WriteLine("Continuar Operando.");
+			}
+			else if (continuar == "C")
+			{
+				Console.WriteLine("Saliendo de la calculadora...");
+				return;
+			}
 
 			//validacion si el usuario desea continuar o no haciendo operaciones
 
